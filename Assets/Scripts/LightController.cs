09f27195dd26b96253939add20fab3f0b02a13ad
Vladimír Hořký <horkyvlad@gmail.com>
2024-11-26 15:53:04 +0100

    initial upload

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LightController : MonoBehaviour
+{
+    [SerializeField] private Light2D _light;
+
+    private void Start()
+    {
+        SetLightColor(new int[] { 1, 0, 1 });
+    }
+
+    public void SetLightColor(int[] colorInput) // 0 or 1
+    {
+        _light.color = new Color(colorInput[0], colorInput[1], colorInput[2]);
+    }
+}
